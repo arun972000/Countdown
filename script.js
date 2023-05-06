f33837d906c2aca6e5d084c8f31d6ed76fe03adb
arun972000 <128138897+arun972000@users.noreply.github.com>
2023-05-06 17:15:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+setTimeout(()=>{
+    let ele=document.getElementById("p1");
+    ele.innerHTML="10";
+    setTimeout(()=>{
+        let ele=document.getElementById("p1");
+        ele.innerHTML="9"
+        setTimeout(()=>{
+            let ele=document.getElementById("p1");
+            ele.innerHTML="8"
+            setTimeout(()=>{
+                let ele=document.getElementById("p1");
+                ele.innerHTML="7"
+                setTimeout(()=>{
+                    let ele=document.getElementById("p1");
+                    ele.innerHTML="6"
+                    setTimeout(()=>{
+                        let ele=document.getElementById("p1");
+                        ele.innerHTML="5"
+                        setTimeout(()=>{
+                            let ele=document.getElementById("p1");
+                            ele.innerHTML="4"
+                            setTimeout(()=>{
+                                let ele=document.getElementById("p1");
+                                ele.innerHTML="3"
+                                setTimeout(()=>{
+                                    let ele=document.getElementById("p1");
+                                    ele.innerHTML="2"
+                                    setTimeout(()=>{
+                                        let ele=document.getElementById("p1");
+                                        ele.innerHTML="1"
+                                        setTimeout(()=>{
+                                            let ele=document.getElementById("p1");
+                                            ele.innerHTML="HAPPY INDEPENDENCE DAY"
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
+
